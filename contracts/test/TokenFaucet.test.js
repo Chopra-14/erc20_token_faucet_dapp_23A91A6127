971372614eca32b611cb8536bf35040ca6473bb4
teddy <sathvikakonakalla14@gmail.com>
2025-12-18 21:47:56 +0530

    Step 1: Project structure initialized

---
+++ contracts/test/TokenFaucet.test.js
@@ -0,0 +1,7 @@
+const { expect } = require("chai");
+
+describe("TokenFaucet", function () {
+  it("Dummy test to satisfy structure", async function () {
+    expect(true).to.equal(true);
+  });
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>ERC-20 Faucet DApp</h1>;
+}
+
+export default App;
